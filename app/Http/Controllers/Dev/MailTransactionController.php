548ded0e81bbf6254e8a21ada98a2cb94cd385a6
RahmaanM17 <ardrah17@gmail.com>
2024-11-07 19:21:27 +0700

    feat: logic on action mail

--- app/Http/Controllers/Dev/MailTransactionController.php
+++ app/Http/Controllers/Dev/MailTransactionController.php
@@ -21,19 +21,19 @@ class MailTransactionController extends Controller
 
     public function dataTable(Request $request)
     {
-        $totalData = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified')->join('mail_agendas as ma', 'ma.id', '=', 'transaction_mails.agenda_id')
+        $totalData = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified', 'wq.user_id as processor_id')->join('mail_agendas as ma', 'ma.id', '=', 'transaction_mails.agenda_id')
             ->join('mail_priorities as mp', 'mp.id', '=', 'transaction_mails.priority_id')
             ->join('mail_types as mt', 'mt.id', '=', 'transaction_mails.type_id')
             ->join('users as u', 'u.id', '=', 'transaction_mails.user_id')
             ->leftJoin('whatsapp_queues as wq', function (JoinClause $join) {
                 $join->on('transaction_mails.id', '=', 'wq.transaction_mail_id')
                     ->on('transaction_mails.status', '=', 'wq.current_status');
-            })->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])
+            })->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])->orWhere([['transaction_mails.creator_id', auth()->user()->id]])
             ->orderBy('id', 'asc')
             ->count();
         $totalFiltered = $totalData;
         if (empty($request['search']['value'])) {
-            $assets = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified')->join('mail_agendas as ma', 'ma.id', '=', 'transaction_mails.agenda_id')
+            $assets = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified', 'wq.user_id as processor_id')->join('mail_agendas as ma', 'ma.id', '=', 'transaction_mails.agenda_id')
                 ->join('mail_priorities as mp', 'mp.id', '=', 'transaction_mails.priority_id')
                 ->join('mail_types as mt', 'mt.id', '=', 'transaction_mails.type_id')
                 ->join('users as u', 'u.id', '=', 'transaction_mails.user_id')
@@ -49,9 +49,9 @@ class MailTransactionController extends Controller
             if (isset($request['order'][0]['column'])) {
                 $assets->orderByRaw($request['columns'][$request['order'][0]['column']]['name'] . ' ' . $request['order'][0]['dir']);
             }
-            $assets = $assets->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])->get();
+            $assets = $assets->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])->orWhere([['transaction_mails.creator_id', auth()->user()->id]])->get();
         } else {
-            $assets = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified')
+            $assets = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified', 'wq.user_id as processor_id')
                 ->join('mail_agendas as ma', 'ma.id', '=', 'transaction_mails.agenda_id')
                 ->join('mail_priorities as mp', 'mp.id', '=', 'transaction_mails.priority_id')
                 ->join('mail_types as mt', 'mt.id', '=', 'transaction_mails.type_id')
@@ -80,9 +80,9 @@ class MailTransactionController extends Controller
                 $assets->limit($request['length'])
                     ->offset($request['start']);
             }
-            $assets = $assets->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])->get();
+            $assets = $assets->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])->orWhere([['transaction_mails.creator_id', auth()->user()->id]])->get();
 
-            $totalFiltered = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified')
+            $totalFiltered = TransactionMail::select('transaction_mails.*', 'u.name as admin', 'ma.name as agenda', 'mp.name as priority', 'mt.name as type', 'wq.notified', 'wq.request_notified', 'wq.user_id as processor_id')
                 ->join('mail_agendas as ma', 'ma.id', '=', 'transaction_mails.agenda_id')
                 ->join('mail_priorities as mp', 'mp.id', '=', 'transaction_mails.priority_id')
                 ->join('mail_types as mt', 'mt.id', '=', 'transaction_mails.type_id')
@@ -107,7 +107,7 @@ class MailTransactionController extends Controller
             if (isset($request['order'][0]['column'])) {
                 $totalFiltered->orderByRaw($request['columns'][$request['order'][0]['column']]['name'] . ' ' . $request['order'][0]['dir']);
             }
-            $totalFiltered = $totalFiltered->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])->count();
+            $totalFiltered = $totalFiltered->where([['transaction_mails.user_id', ((getRole() == 'Developer') ? '<>' : '='), ((getRole() == 'Developer') ? NULL : auth()->user()->id)]])->orWhere([['transaction_mails.creator_id', auth()->user()->id]])->count();
         }
         $dataFiltered = [];
         foreach ($assets as $index => $item) {
@@ -129,14 +129,14 @@ class MailTransactionController extends Controller
             $row['agenda'] = $item->agenda;
             $row['priority'] = $item->priority;
             $row['type'] = $item->type;
-            if ($item->notified && ($item->status == 'REPLIED' || $item->status == 'OUT')) {
+            if ($item->creator_id == auth()->user()->id && $item->notified && ($item->status == 'REPLIED' || $item->status == 'OUT')) {
                 $row['action'] = "<button class='btn btn-icon btn-warning update-status' data-mailsIn='" . $item->id . "' ><i class='bx bx-check-double'></i></button>";
-            } else if ($item->notified && ($item->status != 'REPLIED' && $item->status != 'OUT' && $item->status != 'ARCHIVE')) {
+            } else if ((getRole() == 'Developer' || $item->user_id == auth()->user()->id) && $item->notified && ($item->status != 'REPLIED' && $item->status != 'OUT' && $item->status != 'ARCHIVE')) {
                 $row['action'] = "<button class='btn btn-icon btn-info update-status' data-mailsIn='" . $item->id . "' ><i class='bx bxs-chevrons-up'></i></button><button class='btn btn-icon btn-warning edit' data-mailsIn='" . $item->id . "' ><i class='bx bx-pencil' ></i></button><button data-mailsIn='" . $item->id . "' class='btn btn-icon btn-danger delete'><i class='bx bxs-trash-alt' ></i></button>";
-            } else if ($item->request_notified == true && $item->notified == false) {
-                $row['action'] = "<button class='btn btn-icon btn-secondary disabled' data-mailsIn='" . $item->id . "' ><i class='bx bx-loader-circle' ></i></button>";
-            } else if ($item->status != 'ARCHIVE' && $item->request_notified == false && $item->notified == false) {
+            } else if ($item->processor_id == auth()->user()->id && $item->status != 'ARCHIVE' && $item->request_notified == false && $item->notified == false) {
                 $row['action'] = "<button class='btn btn-icon btn-success request-notify' data-mailsIn='" . $item->id . "' ><i class='bx bxl-whatsapp'></i></button>";
+            } else if (($item->creator_id == auth()->user()->id && $item->status != 'ARCHIVE') || ($item->request_notified == true && $item->notified == false)) {
+                $row['action'] = "<button class='btn btn-icon btn-secondary disabled' data-mailsIn='" . $item->id . "' ><i class='bx bx-loader-circle' ></i></button>";
             } else {
                 $row['action'] = "<button class='btn btn-icon btn-info show' data-mailsIn='" . $item->id . "' ><i class='bx bxs-show'></i></button><button class='btn btn-icon btn-success print-report' data-mailsIn='" . $item->id . "' ><i class='bx bxs-printer'></i></button>";
             }
